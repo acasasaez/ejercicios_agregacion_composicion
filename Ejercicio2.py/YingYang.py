7cb7039342cd9c4b63ad5fcc17f48ebc29d3f0d7
acasasaez <acasasae@myuax.com>
2022-03-25 13:12:11 +0100

    Reescribimos el código para que el mensaje se imprima antes que el interrogante

--- Ejercicio2.py/YingYang.py
+++ Ejercicio2.py/YingYang.py
@@ -13,4 +13,23 @@ print(yang is yin.yang)
 
 del(yang) 
 print("?") 
+print(" ")
+#Reescribimos el código para que que el mensje "Yang destruido" aparezca antres que la interrogación:
+print("RESULTADOS TRAS EL NUEVO CÓDIGO")
+print(" ")
+class Yin: pass 
+class Yang: 
+    def __del__(self): 
+        return "Yang destruido"
+ 
+yin = Yin() 
+yang = Yang() 
+yin.yang = yang 
+ 
+print(yang) 
 
+print(yang is yin.yang) 
+
+print(yang.__del__())
+
+print("?") 
