a8bd5a1518ae986d7314f117d48dec064c5d9ff3
acasasaez <acasasae@myuax.com>
2022-03-28 18:53:33 +0200

    clase empresa

---
+++ Ejercicio1.py/Empresa_Class.py
@@ -0,0 +1,13 @@
+class Empresa: #Clase empresa
+    def __init__(self,name):#La empresa cuenta con un atributo, el nombre
+        self.name = name
+        self.edificios =[]
+        self.personas = []
+        #Mediante atributos inducimos las relaciones de agrgación, 
+        #nuestra empresa está compuesta por edificios y personas (empleados)
+        #sin embargo, si destruímos la empresa esto no implica la desaparición de los edificios y empleados
+        #Creamos los gets y los sets para poder llamar y modificar los elementos del constructor:
+    def set_name(self,name):
+        self.name =name
+    def get_name(self): 
+        return self.name
