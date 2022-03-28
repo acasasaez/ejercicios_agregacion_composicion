d9341e51d70af28100665649994ae8a55de15e32
acasasaez <acasasae@myuax.com>
2022-03-28 18:53:06 +0200

    clase edificio

---
+++ Ejercicio1.py/Edificio_Class.py
@@ -0,0 +1,10 @@
+class Edificio (): 
+    def __init__ (self,name):
+        self.name = name 
+
+    #Creamos los gets y los sets para poder llamar y modificar los elementos del constructor:
+    def set_name(self,name)
+        self.name =name
+    def get_name(self): 
+        return self.name    
+        
