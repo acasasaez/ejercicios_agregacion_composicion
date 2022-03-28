48a821d395f9121d023510ce91469ad9cae7d642
acasasaez <acasasae@myuax.com>
2022-03-28 18:52:49 +0200

    Clase ciudad

---
+++ Ejercicio1.py/Ciudad_Class.py
@@ -0,0 +1,19 @@
+class Ciudad(): #Definimod la clase ciudad 
+    def __init__(self, name) :#La clase ciudad cuenta con el atributo del nombre
+        self.name = name
+        self.edificios =[] #los elementos dde la lista serán objetos de la clase edificio
+        self.persona = []#los elementos de la lista serán de la clase persona
+    #Mediante atributos le inducimos las relaciones de composición
+    #si destruimos la ciudad (cataciclismo) desaparecen las personas y los edificios
+    # (lo tomamos como una relación de composición) 
+
+    #Creamos los gets y los sets para poder llamar y modificar los elementos del constructor:
+    def set_name(self,name):
+        self.name =name
+    def get_name(self): 
+        return self.name
+
+    
+    
+
+    
--- Ejercicio2.py/YingYang.py
+++ Ejercicio2.py/YingYang.py
@@ -15,8 +15,6 @@ del(yang)
 print("?") 
 print(" ")
 #Reescribimos el código para que que el mensje "Yang destruido" aparezca antres que la interrogación:
-print("RESULTADOS TRAS EL NUEVO CÓDIGO")
-print(" ")
 class Yin: pass 
 class Yang: 
     def __del__(self): 
@@ -33,3 +31,6 @@ print(yang is yin.yang)
 print(yang.__del__())
 
 print("?") 
+
+#La razón por la que ocurre esto es porque en el primer caso el valor se almacena en la memoria y en el segundo se le pide que se imprima; 
+#el primer código no da resulatado en todoas las computadoras
