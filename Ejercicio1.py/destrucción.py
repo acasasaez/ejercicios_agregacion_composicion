4fae2b908ac71ac8a78bb5641048ac59dac90d61
acasasaez <acasasae@myuax.com>
2022-04-01 11:36:47 +0200

    creación de objetos

--- Ejercicio1.py/destrucción.py
+++ Ejercicio1.py/destrucción.py
@@ -4,4 +4,15 @@ from Empresa_Class import *
 from persona_Class import*
 from Ciudad_Class import * 
 
+Edificio_A = Edificio()
+Edificio_B = Edificio ()
+Edificio_C = Edificio ()
+La_Empresa = Empresa ()
+La_Ciudad_1 =Ciudad()
+La_Ciudad_2 = Ciudad()
+Empleado_1 = persona ()
+Empleado_2 = persona()
+Empleado_3 = persona()
 
+La_Ciudad_1.set_name("Nueva York")
+La_Ciudad_2.set_name ("")
